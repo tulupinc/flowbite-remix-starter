25870580ae2bd9bebaeb9ad22f277f3ba452dba1
Conner Davis <conner@tulup.io>
2022-05-20 16:50:32 -0700

    feature(route): Add `flowbite-react` kitchen sink to `/`

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "flowbite-remix-starter",
   viewport: "width=device-width,initial-scale=1",
 });
 
